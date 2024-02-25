6a5f2da2fcd45599ed3b349854e35158458e0a53
Write Int <151211283+WriteNaN@users.noreply.github.com>
2024-02-25 17:31:21 +0000

    init discord

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,33 @@
-const server = Bun.serve({
-  port: 3000,
-  fetch(request) {
-    return new Response("Hello, World!");
-  },
+import { Client, GatewayIntentBits, Partials } from "discord.js";
+import { people } from "./emoji";
+
+import "dotenv/config";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.DirectMessageTyping,
+    GatewayIntentBits.GuildMessageTyping
+  ],
+  partials: [Partials.Channel, Partials.Message]
 });
 
-console.log(`Listening on localhost:${server.port}`);
+const prefix = process.env.prefix;
+
+client.once("ready", () => {
+  console.log(`logged in as ${client.user?.username}!`);
+});
+
+client.on("messageCreate", async (message): Promise<any> => {
+  if (message.content.startsWith(prefix) || message.content.split(" ")[0] == `<@${client.user?.id}>`) {
+    message.react(people.slight_smile);
+    return message.reply("hi!");
+  } else {
+    return;
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
