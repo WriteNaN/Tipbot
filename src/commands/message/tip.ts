4fbd73bf2bdc025748d82ee7a7f98e0349c34aec
WriteNaN Hey <nankishre@gmail.com>
2024-02-26 15:31:06 +0000

    aye

--- src/commands/message/tip.ts
+++ src/commands/message/tip.ts
@@ -1,5 +1,19 @@
 import type { Client, Message } from "discord.js";
 
+import { getUser, createUser } from "../../utils/db";
+
 export default function execute(client: Client, message: Message, args: any[]){
-    return message.reply("hi!");
+    const id = parseInt(message.author.id);
+    const user = getUser(id);
+    if (!user) {
+        const done = createUser(id);
+        if (done) {
+            message.reply("y");
+        } else {
+            message.reply("n");
+        }
+        const userX = getUser(id); 
+        return message.reply(user.toString());
+    }
+    return message.reply(user.toString());
 }
